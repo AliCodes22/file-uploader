c690724e2d20957058d3258dbd70c677af1094e1
Ali Ahmed <alitine94@gmail.com>
2025-07-13 05:34:55 -0400

    delete file controller

--- controllers/fileControllers.js
+++ controllers/fileControllers.js
@@ -1,6 +1,6 @@
 import prisma from "../utils/prisma.js";
 import path from "path";
-import { fileURLToPath } from "url";
+import { promises as fs } from "fs";
 
 export const getFile = async (req, res) => {
   const { fileId } = req.params;
@@ -45,5 +45,43 @@ export const downloadFile = async (req, res) => {
     res.download(filePath);
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const deleteFile = async (req, res) => {
+  const { fileId } = req.params;
+
+  try {
+    const file = await prisma.file.findFirst({
+      where: {
+        id: Number(fileId),
+      },
+    });
+
+    if (!file) {
+      return res.status(404).json({
+        message: "file not found",
+      });
+    }
+
+    if (file.userId !== req.user.id) {
+      return res.status(403).json({
+        message: "Unauthorized",
+      });
+    }
+
+    await prisma.file.delete({
+      where: {
+        id: Number(fileId),
+      },
+    });
+
+    await fs.unlink(path.resolve(process.cwd(), "uploads", file.storedName));
+
+    return res.status(200).json({
+      message: "File deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
   }
 };
--- routes/fileRouter.js
+++ routes/fileRouter.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { downloadFile, getFile } from "../controllers/fileControllers.js";
+import {
+  downloadFile,
+  getFile,
+  deleteFile,
+} from "../controllers/fileControllers.js";
+import validateUser from "../middleware/authMiddleware.js";
 
 const fileRouter = Router();
 
+fileRouter.use(validateUser);
+
 fileRouter.get("/:fileId", getFile);
 fileRouter.get("/:fileId/download", downloadFile);
+fileRouter.delete("/:fileId", deleteFile);
 
 export default fileRouter;
